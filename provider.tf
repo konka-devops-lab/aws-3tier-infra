f0ef5966c4837636d0d17e7bcc233597af03cca9
siva <sivaram0434@gmail.com>
2025-06-14 11:30:21 +0530

    remove backed from provider.tf

--- provider.tf
+++ provider.tf
@@ -5,12 +5,7 @@ terraform {
       version = "5.100.0"
     }
   }
-  backend "s3" {
-    bucket         = var.s3_bucket_name
-    key            = "terraform/state"
-    region         = var.aws_region
-    dynamodb_table = var.dynamodb_table_name
-  }
+  backend "s3" {}
 }
 
 provider "aws" {
