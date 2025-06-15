ca4975d3f9a6144d8e3d6cc3f471c17237bec244
siva <sivaram0434@gmail.com>
2025-06-15 15:51:37 +0530

    remove the address

--- modules/elastic_cache/outputs.tf
+++ modules/elastic_cache/outputs.tf
@@ -2,7 +2,7 @@ output "endpoint" {
   description = "The endpoint of the Elastic Cache cluster"
   value       = aws_elasticache_serverless_cache.example.endpoint
 }
-output "address" {
-  description = "The address of the Elastic Cache cluster"
-  value       = aws_elasticache_serverless_cache.example[0].address
-}
+# output "address" {
+#   description = "The address of the Elastic Cache cluster"
+#   value       = aws_elasticache_serverless_cache.example[0].address
+# }
