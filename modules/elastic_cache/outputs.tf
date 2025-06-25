2405bc51b0b067ad96d072222c7058012865e4cb
siva <sivaram0434@gmail.com>
2025-06-25 15:21:00 +0530

    address

--- modules/elastic_cache/outputs.tf
+++ modules/elastic_cache/outputs.tf
@@ -1,4 +1,4 @@
 output "endpoint" {
   description = "The endpoint of the Elastic Cache cluster"
-  value       = aws_elasticache_serverless_cache.example.endpoint.address
+  value       = [for ep in aws_elasticache_serverless_cache.example.endpoint : ep.address]
 }
