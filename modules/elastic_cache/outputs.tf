adba94a7f97e1a3912f8441d75901d2800fbae98
siva <sivaram0434@gmail.com>
2025-06-25 15:19:27 +0530

    add the address of elastic cache

--- modules/elastic_cache/outputs.tf
+++ modules/elastic_cache/outputs.tf
@@ -1,4 +1,4 @@
 output "endpoint" {
   description = "The endpoint of the Elastic Cache cluster"
-  value       = aws_elasticache_serverless_cache.example[0].endpoint
+  value       = aws_elasticache_serverless_cache.example.endpoint.address
 }
