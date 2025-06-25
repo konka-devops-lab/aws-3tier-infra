a66baeca96ea119ca597774135f0167c599e0636
siva <sivaram0434@gmail.com>
2025-06-25 15:16:37 +0530

    get only the address

--- modules/elastic_cache/outputs.tf
+++ modules/elastic_cache/outputs.tf
@@ -1,8 +1,4 @@
 output "endpoint" {
   description = "The endpoint of the Elastic Cache cluster"
-  value       = aws_elasticache_serverless_cache.example.endpoint
+  value       = aws_elasticache_serverless_cache.example[0].endpoint
 }
-# output "address" {
-#   description = "The address of the Elastic Cache cluster"
-#   value       = aws_elasticache_serverless_cache.example[0].address
-# }
